4e227758e23415f851eae40d3250393f37f74ca7
waylen94 <763027562@qq.com>
2019-10-25 21:52:47 +0800

    dealing with side bar structure

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,28 +1,236 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="container">
+
+
+        <div class="sidenav">
+         <div class="login-main-text">
+                <img class="img-circle img-responsive" src="http://api.adorable.io/avatars/300/abott@adorable.png">
+                    <div>
+                        <h2> <span itemprop="name">Weilun Liu</span></h2>
+                        <p></p>
+                        <p></p>
+                        <p><i class="fa fa-map-marker"></i> <span itemprop="addressRegion">Brisbane, QLD</span></p>
+                        <p itemprop="email"> <i class="fa fa-envelope"> </i> <a href="mailto:you@somedomain.com">valenliu1994@gmail.com</a> </p>
+                    </div>
+         </div>
+      </div>
+
+
+      <div class="main">
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome from Bootstrap</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a></p>
+      </div>
+    </div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome from Font Awesome</h1>
+        <p>Get vector icons and social logos on your website with Font Awesome, the web’s most popular icon set and toolkit.</p>
+        <p>
+          <i class="fa fa-camera"></i>
+          <i class="fa fa-camera fa-2x"></i>
+          <i class="fa fa-camera fa-3x"></i>
+          <i class="fa fa-camera fa-4x"></i>
+          <i class="fa fa-camera fa-5x"></i>
+          <i class="fa fa-spinner fa-spin"></i>
+        </p>
+      </div>
+    </div>
+
+
+
+
+    <div class="how-section1">
+                    <div class="row">
+                        <div class="col-md-6 how-img">
+                            <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                        <div class="col-md-6">
+                            <h4>Find rewarding projects</h4>
+                                        <h4 class="subheading">GetLance is a great place to find more clients, and to run and grow your own freelance business.</h4>
+                        <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
+                                            Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.
+                                            More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>Get hired quickly</h4>
+                                        <h4 class="subheading">GetLance makes it easy to connect with clients and begin doing great work.</h4>
+                                        <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
+                                            Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
+                                            Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 how-img">
+                             <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                        <div class="col-md-6">
+                            <h4>Work efficiently, effectively.</h4>
+                                        <h4 class="subheading">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
+                                        <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
+                                            Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
+                                            Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>Get paid on time</h4>
+                                        <h4 class="subheading">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
+                                        <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
+                                            Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
+                                            Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>User Name</label>
+                     <input type="text" class="form-control" placeholder="User Name">
+                  </div>
+                  <div class="form-group">
+                     <label>Password</label>
+                     <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-black">Login</button>
+                  <button type="submit" class="btn btn-secondary">Register</button>
+               </form>
+            </div>
+         </div>
+      </div>
+
+
+
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+$fa-font-path: "~font-awesome/fonts/";
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
+
+body > .container { margin-top: 15px;}
+
+
+.how-section1{
+    margin-top:-15%;
+    padding: 10%;
+}
+.how-section1 h4{
+    color: #ffa500;
+    font-weight: bold;
+    font-size: 30px;
+}
+.how-section1 .subheading{
+    color: #3931af;
+    font-size: 20px;
+}
+.how-section1 .row
+{
+    margin-top: 10%;
+}
+.how-img
+{
+    text-align: center;
+}
+.how-img img{
+    width: 40%;
+}
+
+
+.main-head{
+    height: 150px;
+    background: #FFF;
+
+}
+
+.sidenav {
+    height: 100%;
+    background-color: #000;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+
+.main {
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+}
+
+@media screen and (max-width: 450px) {
+    .login-form{
+        margin-top: 10%;
+    }
+
+    .register-form{
+        margin-top: 10%;
+    }
+}
+
+@media screen and (min-width: 768px){
+    .main{
+        margin-left: 25%;
+    }
+
+    .sidenav{
+        width: 25%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+    }
+
+    .login-form{
+        margin-top: 80%;
+    }
+
+    .register-form{
+        margin-top: 20%;
+    }
+}
+
+
+.login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #fff;
+}
+
+.login-main-text h2{
+    font-weight: 300;
+}
+
+.btn-black{
+    background-color: #000 !important;
+    color: #fff;
 }
 </style>
