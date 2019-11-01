fc7b80fc179bea8edb2c0e897c57537b855b543f
waylen94 <763027562@qq.com>
2019-11-01 16:39:35 +0800

    resume framework

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="container">
 
-
-        <div class="sidenav">
+<!-- Side Bar for cotraction -->
+<div class="sidenav">
          <div class="login-main-text">
                 <img class="img-circle img-responsive" src="http://api.adorable.io/avatars/300/abott@adorable.png">
                     <div>
                         <h2> <span itemprop="name">Weilun Liu</span></h2>
-                        <p></p>
-                        <p></p>
+
+                        <p> <ul class="social-network social-circle">
+                        <li><a href="https://github.com/waylen94" class="icoGitbub" title="Github"><i class="fa fa-github"></i></a></li>
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+
+                        <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="https://waylen94.github.io/Diary/" class="icoDiary" title="Diary"><i class="fa fa-calendar"></i></a></li>
+
+                    </ul>
+
+                  </p>
                         <p><i class="fa fa-map-marker"></i> <span itemprop="addressRegion">Brisbane, QLD</span></p>
                         <p itemprop="email"> <i class="fa fa-envelope"> </i> <a href="mailto:you@somedomain.com">valenliu1994@gmail.com</a> </p>
+
+
+
                     </div>
          </div>
-      </div>
+</div>
 
-
-      <div class="main">
+<!-- Content -->
+<div class="main">
     <div class="jumbotron">
       <div class="container">
-        <h1>Welcome from Bootstrap</h1>
-        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h1>Education</h1>
+        <p></p>
         <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a></p>
       </div>
     </div>
     <div class="jumbotron">
       <div class="container">
-        <h1>Welcome from Font Awesome</h1>
+        <h1>Work Experience</h1>
         <p>Get vector icons and social logos on your website with Font Awesome, the web’s most popular icon set and toolkit.</p>
         <p>
           <i class="fa fa-camera"></i>
@@ -42,7 +55,10 @@
 
 
 
-    <div class="how-section1">
+
+    <div class="projectexhibition-section">
+      <h1>Project Exhibition</h1>
+
                     <div class="row">
                         <div class="col-md-6 how-img">
                             <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt=""/>
@@ -55,6 +71,7 @@
                                             More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-6">
                             <h4>Get hired quickly</h4>
@@ -67,6 +84,7 @@
                             <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt=""/>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-6 how-img">
                              <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt=""/>
@@ -79,6 +97,7 @@
                                             Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-6">
                             <h4>Get paid on time</h4>
@@ -91,33 +110,9 @@
                             <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt=""/>
                         </div>
                     </div>
+
                 </div>
-
-
-
-
-
-
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-               <form>
-                  <div class="form-group">
-                     <label>User Name</label>
-                     <input type="text" class="form-control" placeholder="User Name">
-                  </div>
-                  <div class="form-group">
-                     <label>Password</label>
-                     <input type="password" class="form-control" placeholder="Password">
-                  </div>
-                  <button type="submit" class="btn btn-black">Login</button>
-                  <button type="submit" class="btn btn-secondary">Register</button>
-               </form>
-            </div>
-         </div>
       </div>
-
-
-
   </div>
 
 
@@ -138,23 +133,24 @@ $fa-font-path: "~font-awesome/fonts/";
 body > .container { margin-top: 15px;}
 
 
-.how-section1{
-    margin-top:-15%;
-    padding: 10%;
+.projectexhibition-section{
+    margin-top:2%;
+    padding: 5%;
+
+    h4{
+        color: #ffa500;
+        font-weight: bold;
+        font-size: 30px;
+    }
+    .subheading{
+        color: #3931af;
+        font-size: 20px;
+    }
+    .row{
+        margin-top: 10%;
+    }
 }
-.how-section1 h4{
-    color: #ffa500;
-    font-weight: bold;
-    font-size: 30px;
-}
-.how-section1 .subheading{
-    color: #3931af;
-    font-size: 20px;
-}
-.how-section1 .row
-{
-    margin-top: 10%;
-}
+
 .how-img
 {
     text-align: center;
@@ -233,4 +229,89 @@ body > .container { margin-top: 15px;}
     background-color: #000 !important;
     color: #fff;
 }
+
+
+/*=========================
+  Icons
+ ================= */
+
+/* footer social icons */
+ul.social-network {
+  list-style: none;
+  display: inline;
+  margin-left:0 !important;
+  padding: 0;
+}
+ul.social-network li {
+  display: inline;
+  margin: 0 5px;
+}
+
+
+/* footer social icons */
+.social-network a.icoGitbub:hover {
+  background-color: #F56505;
+}
+.social-network a.icoFacebook:hover {
+  background-color:#3B5998;
+}
+.social-network a.icoTwitter:hover {
+  background-color:#33ccff;
+}
+.social-network a.icoGoogle:hover {
+  background-color:#BD3518;
+}
+.social-network a.icoDiary:hover {
+  background-color:#0590B8;
+}
+.social-network a.icoLinkedin:hover {
+  background-color:#007bb7;
+}
+.social-network a.icoGitbub:hover i, .social-network a.icoFacebook:hover i, .social-network a.icoTwitter:hover i,
+.social-network a.icoGoogle:hover i, .social-network a.icoDiary:hover i, .social-network a.icoLinkedin:hover i {
+  color:#fff;
+}
+a.socialIcon:hover, .socialHoverClass {
+  color:#44BCDD;
+}
+
+.social-circle li a {
+  display:inline-block;
+  position:relative;
+  margin:0 auto 0 auto;
+  -moz-border-radius:50%;
+  -webkit-border-radius:50%;
+  border-radius:50%;
+  text-align:center;
+  width: 50px;
+  height: 50px;
+  font-size:20px;
+}
+.social-circle li i {
+  margin:0;
+  line-height:50px;
+  text-align: center;
+}
+
+.social-circle li a:hover i, .triggeredHover {
+  -moz-transform: rotate(360deg);
+  -webkit-transform: rotate(360deg);
+  -ms--transform: rotate(360deg);
+  transform: rotate(360deg);
+  -webkit-transition: all 0.2s;
+  -moz-transition: all 0.2s;
+  -o-transition: all 0.2s;
+  -ms-transition: all 0.2s;
+  transition: all 0.2s;
+}
+.social-circle i {
+  color: #fff;
+  -webkit-transition: all 0.8s;
+  -moz-transition: all 0.8s;
+  -o-transition: all 0.8s;
+  -ms-transition: all 0.8s;
+  transition: all 0.8s;
+}
+
+
 </style>
