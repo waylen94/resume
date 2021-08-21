ee53dad8da056ddf6bdec18222bef734f52dca3e
Waylen Liu <763027562@qq.com>
2021-08-21 15:39:59 +1000

    first change for testing Vue project

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -330,9 +330,9 @@
                       <div class="row">
                       <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
                         <p>Waylen Resume Plateform</p>
-                        <p class="h6">&copy All right Reversed.</p>
+                        <p class="h6">& copy All right Reversed.</p>
                       </div>
-                      </hr>
+                      
                       </div>
       </div>
 
